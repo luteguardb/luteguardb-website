9b05ae3020e3de583d9594bf6cdf52b3a3a0bffb
luteguardb <luteguard.b@gmail.com>
2026-09-18 10:04:53 +0900

    Update SiteFooter.js

--- components/SiteFooter.js
+++ components/SiteFooter.js
@@ -3,22 +3,31 @@ import Link from "next/link";
 export default function SiteFooter() {
   return (
     <footer className="site-footer">
+
       <div>
-        <div className="footer-brand">LUTEGUARD-B™</div>
-        <div className="footer-small">BOTANICAL SCIENCE FOR BRIGHTER TOMORROWS</div>
+        <div className="footer-brand">
+          LUTEGUARD-B™
+        </div>
+
+        <div className="footer-small">
+          BOTANICAL SCIENCE FOR BRIGHTER TOMORROWS
+        </div>
       </div>
 
       <div className="footer-links">
         <Link href="/archive">ARCHIVE</Link>
         <Link href="/our-story">OUR STORY</Link>
         <Link href="/luteguard-b">LUTEGUARD-B™</Link>
+        <Link href="/shop">SHOP</Link>
         <Link href="/contact">CONTACT</Link>
       </div>
 
       <div className="footer-small footer-right">
-        Ganghwa · Gyodong Island · Korea<br />
+        Ganghwa · Gyodong Island · Korea
+        <br />
         © 2026 LUTEGUARD-B™
       </div>
+
     </footer>
   );
 }
